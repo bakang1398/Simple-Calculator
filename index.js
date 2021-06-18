65a292bd7dcf2266f7072fe24b2dc973e4c2a495
bakang1398 <bakangsebetlela@gmail.com>
2021-06-18 04:02:18 +0200

    Added functions to sum and subtract two numbers

---
+++ index.js
@@ -0,0 +1,20 @@
+// Addition of two numbers function
+
+function add(a,b) {
+    let sum = a + b;
+
+    document.write("The answer is: " + sum, "<br>");
+}
+
+add(1989,1998);
+
+// Subtraction of two numbers function
+
+function subtract(c, d) {
+    let difference = c - d;
+    console.log(difference);
+
+    document.write(difference, "<br>");
+}
+
+subtract(8741, 1478);
